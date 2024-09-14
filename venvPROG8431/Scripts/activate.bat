8c4b134d03b27a7dd379a32e8eefa0979ee880a5
Keerthi Gonuguntla <keerthi.mavks@gmail.com>
2024-09-14 15:05:34 -0400

    Created virtual Environment and run the project Moved requirements.txt file in base directory ignored user specific environment changes in gitignore

--- venvPROG8431/Scripts/activate.bat
+++ venvPROG8431/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Conestoga\PROG8431-DAM\PROG8431\venvPROG8431
+set VIRTUAL_ENV=C:\Users\ADMIN\Documents\data analysis\PROG8431\venvPROG8431
 
 if not defined PROMPT set PROMPT=$P$G
 
